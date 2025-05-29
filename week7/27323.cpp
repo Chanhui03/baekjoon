52fd9347e24abeb2747a6b6e0d2be5bf40c70f37
ChanHuiKwon <108207018+Chanhui03@users.noreply.github.com>
2025-05-29 22:20:14 +0900

    직사각형

---
+++ week7/27323.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// 직사각형
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int a, b;
+  cin >> a;
+  cin >> b;
+
+  cout << a*b << '\n';
+
+  return 0;
+}

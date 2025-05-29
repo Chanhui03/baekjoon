4a837e4aa7ae68b292870617610e2fd3f3f7a331
ChanHuiKwon <108207018+Chanhui03@users.noreply.github.com>
2025-05-29 22:19:27 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ week7/24262.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// 알고리즘 수업 - 알고리즘의 수행 시간 1
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int n;
+  cin >> n;
+
+  cout << 1 << '\n' << 0;
+
+  return 0;
+}

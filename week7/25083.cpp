29a3938399794fc65e30717921f206ede12d6375
ChanHuiKwon <108207018+Chanhui03@users.noreply.github.com>
2025-05-29 22:19:53 +0900

    새싹

---
+++ week7/25083.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// 새싹
+int main() {
+  ios::sync_with_stdio(0);
+  cout.tie(0);
+
+  cout << "         ,r'\"7\n";
+  cout << "r`-_   ,'  ,/\n";
+  cout << " \\. \". L_r'\n";
+  cout << "   `~\\/\n";
+  cout << "      |\n";
+  cout << "      |\n";
+
+
+  return 0;
+}

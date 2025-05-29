b15e9ff92567eca8fca09fa48e5c6c2b1f7ec33d
ChanHuiKwon <108207018+Chanhui03@users.noreply.github.com>
2025-05-29 22:18:19 +0900

    아스키 코드

---
+++ week7/11654.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// 아스키 코드
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  char a;
+  cin >> a;
+
+  cout << int(a);
+
+  return 0;
+}

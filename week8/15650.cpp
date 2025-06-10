200a19cc44719fc0c962abf1762b8b6df991c666
ChanHuiKwon <108207018+Chanhui03@users.noreply.github.com>
2025-06-10 13:18:28 +0900

    N과 M (2)

---
+++ week8/15650.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+int n, m;
+int arr[9] = {0, };
+bool visited[9] = {0, };
+
+void dfs(int num, int cnt) {
+  if (cnt == m) {
+    for (int i = 0; i < m; i++) {
+      cout << arr[i] << ' ';
+    }
+    cout << '\n';
+    return;
+  }
+  for (int i = num; i <= n; i++) {
+    if(!visited[i]) {
+      visited[i] = true;
+      arr[cnt] = i;
+      dfs(i + 1, cnt + 1);
+      visited[i] = false;
+    }
+  }
+}
+
+// N과 M (2)
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> n >> m;
+
+  dfs(1, 0);
+
+  return 0;
+}
